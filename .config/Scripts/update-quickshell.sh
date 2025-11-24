2a37e4b752d5666f45c559e46f9c09dcf669a6b9
David Metzler <david-metzler@web.de>
2025-11-24 14:58:04 +0100

    Add update-quickshell-script

---
+++ .config/Scripts/update-quickshell.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git fetch quickshell_upstream main
+git branch -d quickshell_upstream_main
+git checkout -b quickshell_upstream_main quickshell_upstream/main
+git subtree split -P dots/.config/quickshell -b quickshell_subfolder
+git checkout master
+git subtree merge --prefix=.config/quickshell quickshell_subfolder
